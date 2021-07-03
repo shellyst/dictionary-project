409b7d8f5c548874bdd2515ab481c5c33a70542f
shellyst <michellechristine.s91@gmail.com>
2021-07-03 19:15:11 -0400

    Added a search bar to track searched words.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
 import book from "./book.jpg";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary Application</header>
-
-      <br />
-      <img src={book} className="Book-logo" alt="book" />
+      <div className="container">
+        <header className="App-header">Dictionary Application</header>
+        <main>
+          <Dictionary />
+        </main>
+        <br />
+        <img src={book} className="Book-logo" alt="book" />
+        <footer className="text-center">
+          <small>Coded by: Michelle Stone</small>
+        </footer>
+      </div>
     </div>
   );
 }
