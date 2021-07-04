60a78bb01369ec546428669a9af38f2e189dae31
shellyst <michellechristine.s91@gmail.com>
2021-07-04 19:30:06 -0400

    Removed image.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import book from "./book.jpg";
+
 import Dictionary from "./Dictionary";
 
 export default function App() {
@@ -11,7 +11,7 @@ export default function App() {
           <Dictionary />
         </main>
         <br />
-        <img src={book} className="Book-logo" alt="book" />
+
         <footer className="text-center">
           <small>Coded by: Michelle Stone</small>
         </footer>
