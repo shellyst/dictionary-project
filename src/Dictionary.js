9d514530fdf34fb508a31859f7857c3414dff80b
shellyst <michellechristine.s91@gmail.com>
2021-07-04 21:37:58 -0400

    Added event.preventDefault

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,9 +12,9 @@ export default function Dictionary() {
     setResults(response.data[0]);
   }
 
-  function search() {
+  function search(event) {
+    event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-
     axios.get(apiUrl).then(handleResponse);
   }
 
